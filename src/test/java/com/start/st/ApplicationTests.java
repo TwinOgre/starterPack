36c375d4bb657605240a4230f7d6db0802e11af7
twinogre <glad2you@naver.com>
2024-01-17 18:50:50 +0900

    testCode_created

--- src/test/java/com/start/st/ApplicationTests.java
+++ src/test/java/com/start/st/ApplicationTests.java
@@ -1,6 +1,8 @@
 package com.start.st;
 
+import com.start.st.domain.article.service.ArticleService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -10,4 +12,11 @@ class ApplicationTests {
 	void contextLoads() {
 	}
 
+	@Autowired
+	ArticleService articleService;
+	@Test
+	void contextLoads2(){
+		articleService.create("subject1","content1");
+
+	}
 }
